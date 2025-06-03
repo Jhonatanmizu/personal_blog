c570872999329991b85733a5374bbd33dc94c47c
jhonatanmizu <natanjesuss20@gmail.com>
2025-06-03 20:48:51 -0300

    feat: create app_setup processors

---
+++ src/app_setup/context_processors.py
@@ -0,0 +1,20 @@
+from .models import SiteSetup
+
+
+def app_setup(_):
+    setup = SiteSetup.objects.order_by('-id').first()
+
+    if not setup:
+        return {
+            'app_setup': 'app_setup'
+        }
+
+    return {
+        'app_setup': 'app_setup',
+        "title": setup.title,
+        "description": setup.description,
+        "show_header": setup.show_header,
+        "show_search": setup.show_search,
+        "show_menu": setup.show_menu,
+        "show_description": setup
+    }
