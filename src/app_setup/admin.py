937073e764ffb293a2f58e935ed19e8b52545251
jhonatanmizu <natanjesuss20@gmail.com>
2025-06-03 20:09:16 -0300

    feat: create site setup model

--- src/app_setup/admin.py
+++ src/app_setup/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
-from .models import MenuLink
+from .models import MenuLink, SiteSetup
 
 
 @admin.register(MenuLink)
@@ -8,3 +9,11 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'text', 'url_or_path', 'id'
     list_display_links = 'text', 'url_or_path', 'id'
     search_fields = "id", "text", "url_or_path"
+
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return not SiteSetup.objects.exists()
