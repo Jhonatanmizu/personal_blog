11c59f0bc7e125e05abb4ecc42b8d01bef8d271b
jhonatanmizu <natanjesuss20@gmail.com>
2025-06-10 21:32:23 -0300

    feat: create post detail view and page detail view

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def index(request: HttpRequest):
     return render(request, 'blog/pages/index.html')
+
+
+def post_detail(request: HttpRequest):
+    return render(request, 'blog/pages/post.html')
+
+
+def page_detail(request: HttpRequest):
+    return render(request, 'blog/pages/page.html')
