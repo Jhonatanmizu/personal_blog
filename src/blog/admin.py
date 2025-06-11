6aa9bbaaa02a20f8e237dcc5ac7c22e9ecbb6ace
jhonatanmizu <natanjesuss20@gmail.com>
2025-06-10 22:11:56 -0300

    feat: create page, tags and categories models

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Page, Tags
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    list_per_page = 10
+    ordering = ('-id',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    list_per_page = 10
+    ordering = ('-id',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    search_fields = ('title', 'slug')
+    list_per_page = 10
+    ordering = ('-id',)
+    prepopulated_fields = {'slug': ('title',)}
--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+from app.utils.rand import slugify_new
+
+
+class Tags(models.Model):
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=255,
+                            unique=True, default=None, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f'{slugify_new(self.name)}'
+        return super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, default=None,
+                            blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f'{slugify_new(self.name)}'
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Page(models.Model):
+
+    class Meta:
+        verbose_name = "Page"
+        verbose_name_plural = "Pages"
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(max_length=255, default=None,
+                            blank=True, unique=True)
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f'{slugify_new(self.title)}'
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.title}'
