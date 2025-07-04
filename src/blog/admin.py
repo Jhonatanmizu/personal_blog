1d98881038a0ad93a7a904882bd866ff3c9c9227
jhonatanmizu <natanjesuss20@gmail.com>
2025-07-04 00:23:08 -0300

    feat: add post to admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,30 +1,81 @@
+from typing import Any
+
 from django.contrib import admin
+from django.http.request import HttpRequest
 
-from .models import Category, Page, Tags
+from .models import Category, Page, Post, Tag
 
 
-@admin.register(Tags)
+@admin.register(Tag)
 class TagsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    search_fields = ('name', 'slug')
+    list_display = ("name", "slug")
+    search_fields = ("name", "slug")
     list_per_page = 10
-    ordering = ('-id',)
-    prepopulated_fields = {'slug': ('name',)}
+    ordering = ("-id",)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    search_fields = ('name', 'slug')
+    list_display = ("name", "slug")
+    search_fields = ("name", "slug")
     list_per_page = 10
-    ordering = ('-id',)
-    prepopulated_fields = {'slug': ('name',)}
+    ordering = ("-id",)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug')
-    search_fields = ('title', 'slug')
+    list_display = ("title", "slug")
+    search_fields = ("title", "slug")
     list_per_page = 10
-    ordering = ('-id',)
-    prepopulated_fields = {'slug': ('title',)}
+    ordering = ("-id",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "is_published",
+        # "created_by",
+    )
+    list_display_links = ("title",)
+    search_fields = (
+        "id",
+        "slug",
+        "title",
+        "excerpt",
+        "content",
+    )
+    list_per_page = 50
+    list_filter = (
+        "category",
+        "is_published",
+    )
+    list_editable = ("is_published",)
+    ordering = ("-id",)
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+        "created_by",
+        "updated_by",
+    )
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+    autocomplete_fields = (
+        "tags",
+        "category",
+    )
+
+    def save_model(
+        self, request: HttpRequest, obj: Post, form: Any, change: Any
+    ) -> None:
+        if change:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+        # obj.save()
+        return super().save_model(request, obj, form, change)
