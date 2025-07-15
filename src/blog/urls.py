bc3f4e5f6de6358a0a82d9378cf3e9f0d70c6731
jhonatanmizu <natanjesuss20@gmail.com>
2025-07-14 21:19:08 -0300

    feat: create path urls

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path('', views.index, name='blog'),
+    path("post/<slug:slug>/", views.index, name="post"),
+    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
+    path("page/<int:pk>/", views.page_detail, name="page_detail"),
+    path("", views.index, name="blog"),
 ]
