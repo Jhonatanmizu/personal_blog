3cb986b90b57a7dc0016793e52312e4b9d294a67
jhonatanmizu <natanjesuss20@gmail.com>
2025-07-14 21:18:54 -0300

    feat: add posts in index

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,14 +1,32 @@
+from django.core.paginator import Paginator
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from blog.models import Post
+
+POST_PER_PAGE = 9
+
+
+def filter_posts():
+    return Post.objects.filter(is_published=True).order_by("-pk")
+
 
 def index(request: HttpRequest):
-    return render(request, 'blog/pages/index.html')
+    posts = filter_posts()
+    paginator = Paginator(posts, POST_PER_PAGE)
+    page_obj = paginator.get_page(request.GET.get("page", 1))
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        },
+    )
 
 
 def post_detail(request: HttpRequest):
-    return render(request, 'blog/pages/post.html')
+    return render(request, "blog/pages/post.html")
 
 
 def page_detail(request: HttpRequest):
-    return render(request, 'blog/pages/page.html')
+    return render(request, "blog/pages/page.html")
